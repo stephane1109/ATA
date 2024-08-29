6ff13732f3448c4fbcdfe149cf1961ace8b835d1
Stéphane Meurisse <141574089+stephane1109@users.noreply.github.com>
2024-08-29 18:08:35 +0200

    Update gestion_doublons.py

--- gestion_doublons.py
+++ gestion_doublons.py
@@ -1,11 +1,11 @@
 ##########################################
 # Projet : Analyse Textuelle Avancée (ATA)
 # Auteur : Stéphane Meurisse
-# Contact : stephane.meurisse@example.com
+# Contact : stephane.meurisse@gmail.com
 # Site Web : https://www.codeandcortex.fr
 # LinkedIn : https://www.linkedin.com/in/st%C3%A9phane-meurisse-27339055/
 # Date : 22 août 2024
-# Version : 0.1.0-beta
+# Version : 0.1.1-beta
 # Licence : Ce programme est un logiciel libre : vous pouvez le redistribuer selon les termes de la Licence Publique Générale GNU v3
 ##########################################
 

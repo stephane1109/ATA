4b4528410599daa8af792adf4755fc60f0b7085c
Stéphane Meurisse <141574089+stephane1109@users.noreply.github.com>
2024-08-29 17:59:19 +0200

    Update lda.py

--- lda.py
+++ lda.py
@@ -1,3 +1,15 @@
+##########################################
+# Projet : Analyse Textuelle Avancée (ATA)
+# Auteur : Stéphane Meurisse
+# Contact : stephane.meurisse@gmail.com
+# Site Web : https://www.codeandcortex.fr
+# LinkedIn : https://www.linkedin.com/in/st%C3%A9phane-meurisse-27339055/
+# Date : 22 août 2024
+# Version : 0.1.0-beta
+# Licence : Ce programme est un logiciel libre : vous pouvez le redistribuer selon les termes de la Licence Publique Générale GNU v3
+##########################################
+
+
 import streamlit as st
 import spacy
 import pandas as pd

a753827bdeba8d700ae5478c75d99b71f571cc78
Stéphane Meurisse <141574089+stephane1109@users.noreply.github.com>
2024-08-29 18:00:54 +0200

    Update tfidf.py

--- tfidf.py
+++ tfidf.py
@@ -1,7 +1,7 @@
 ##########################################
 # Projet : Analyse Textuelle Avancée (ATA)
 # Auteur : Stéphane Meurisse
-# Contact : stephane.meurisse@example.com
+# Contact : stephane.meurisse@gmail.com
 # Site Web : https://www.codeandcortex.fr
 # LinkedIn : https://www.linkedin.com/in/st%C3%A9phane-meurisse-27339055/
 # Date : 22 août 2024

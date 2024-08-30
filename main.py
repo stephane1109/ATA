c788eca3bfe7399490dec3afceb95d7cee3f6fdd
Stéphane Meurisse <141574089+stephane1109@users.noreply.github.com>
2024-08-30 13:29:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@
 # Contact : stephane.meurisse@gmail.com
 # Site Web : https://www.codeandcortex.fr
 # LinkedIn : https://www.linkedin.com/in/st%C3%A9phane-meurisse-27339055/
-# Date : 29 août 2024
-# Version : 0.2.1-beta
+# Date : 30 août 2024
+# Version : 0.3.1-beta
 # Licence : Ce programme est un logiciel libre : vous pouvez le redistribuer selon les termes de la Licence Publique Générale GNU v3
 ##########################################
 
@@ -25,6 +25,7 @@ from youtube_transcription import afficher_interface_youtube_transcription
 from mp4tomp3 import afficher_interface_mp4_to_mp3
 from html_to_text import afficher_interface_html_to_text
 from france_inter_audio import afficher_interface_france_inter_audio
+from mp3_to_text import afficher_interface_audio_to_text_whisper
 from faq import afficher_faq
 import os
 
@@ -35,7 +36,7 @@ st.title("ATA - Analyse Textuelle Avancée")
 
 # Sous-titre avec la version et les liens
 st.markdown(
-    "**ATA - Version 0.1.0 Beta - [Dépôt Github](https://github.com/stephane1109/ATA) - [Site web](http://www.codeandcortex.fr)**")
+    "**ATA - Version 0.3.1 Beta - [Dépôt Github](https://github.com/stephane1109/ATA) - [Site web](http://www.codeandcortex.fr)**")
 
 # Menu horizontal principal
 selected = option_menu(
@@ -50,17 +51,26 @@ if selected == "Accueil":
     st.subheader("Bienvenue dans l'application d'analyse textuelle avancée.")
 
     st.markdown("""
-    **Version Bêta 0.1.0**
+    **Version Bêta 0.3.1**
 
     Cette interface regroupe plusieurs scripts Python que j'ai développés individuellement au fil du temps, maintenant réunis pour vous offrir des outils d'analyse textuelle.
+    
+    ### Formatage des documents
 
-    Vous y trouverez des algorithmes NLP courants comme **TF-IDF**, **K-MEANS**, **LDA** et bien d'autres. L'idée est de vous donner une plateforme pratique pour explorer, analyser et interpréter vos données textuelles.
+    Les documents utilisés pour les tests dans ce projet sont formatés de manière à se rapprocher au maximum du format utilisé dans le logiciel IRAMUTEQ.
+    Cela vous permettra de passer facilement d'IRAMUTEQ aux tests des algorithmes NLP, tels que KMeans, LDA, ou encore la CAH.
 
+    ### Interface ATA
+
+    L'interface ATA a été conçue comme un outil qui combine à la fois le traitement des données (scraping, transformatio...) et l'analyse textuelle à l'aide d'algorithmes NLP courants comme **TF-IDF**, **K-MEANS**, **LDA** et bien d'autres.
+    Elle vous permet d'effectuer ces tâches sans nécessiter de connaissances approfondies en programmation, le tout à travers une interface (conviviale) accessible via votre navigateur.
+    
+    L'idée est de vous donner une plateforme pratique pour explorer, analyser et interpréter vos données textuelles.
+
+    ### Modèle NLP
     Actuellement, le modèle **SpaCy large** en français est utilisé pour le traitement du texte (NLP), offrant une "relative" précision et (surtout) exploitable avec n'importe quel Mac/PC. 
 
-    Mais restez à l'affût, car cette version Bêta est encore en développement et pourrait s'enrichir d'autres fonctionnalités et améliorations.
-
-    Explorez les différentes options du menu pour commencer et n'oubliez pas de me faire part de vos retours. C'est grâce à vous que cette application pourra s'améliorer !
+    Restez à l'affût, car cette version Bêta est encore en développement et pourrait s'enrichir d'autres fonctionnalités et améliorations !
 
     """)
 
@@ -70,7 +80,7 @@ if selected == "Accueil":
 # Préparation des Données (renommé "Data")
 elif selected == "Data":
     prep_option = option_menu(
-        None, ["Europresse html to text", "PDF to text", "Recherche de doublons", "MP4 to MP3", "Voice to text"],
+        None, ["Europresse html to text", "PDF to Text", "Search doublons", "MP4 to MP3", "MP3 to Text", "Youtube to Text"],
         icons=['file-earmark', 'file-pdf', 'search', 'mic'],
         menu_icon="file-earmark", default_index=0, orientation="horizontal"
     )
@@ -78,7 +88,7 @@ elif selected == "Data":
     if prep_option == "Europresse html to text":
         afficher_interface_europresse()
 
-    elif prep_option == "PDF to text":
+    elif prep_option == "PDF to Text":
         traitement_pdf()
 
     elif prep_option == "Recherche de doublons":
@@ -87,8 +97,11 @@ elif selected == "Data":
     elif prep_option == "MP4 to MP3":
         afficher_interface_mp4_to_mp3()
 
-    elif prep_option == "Voice to text":
+    elif prep_option == "Youtube to Text":
         afficher_interface_youtube_transcription()
+
+    elif prep_option == "MP3 to Text":
+        afficher_interface_audio_to_text_whisper()
 
 # Cooccurrences
 elif selected == "Cooccu":
@@ -127,10 +140,11 @@ elif selected == "Scraper":
     elif scraper_option == "Audio France Inter":
         afficher_interface_france_inter_audio()
 
-# Audio France Inter
+# France Inter Audio
 elif selected == "Audio France Inter":
     afficher_interface_france_inter_audio()
 
 # FAQ
 elif selected == "FAQ":
     afficher_faq()
+
